31dde3460a25a28f6ea369167bf2d5ac5259433d
VexiDev <79677251+VexiDev@users.noreply.github.com>
2025-03-04 15:48:19 -0500

    feat(server): Refactored to correctly run on IPPROTO and listen for received data
    Using net.Address.parseIp4 simplifies the creation of the sockaddr type when binding.
    The posix.listen() operate on the Transport layer so cannot be used on a raw socket without it.

--- src/main.zig
+++ src/main.zig
@@ -1,26 +1,36 @@
 const std = @import("std");
 const posix = std.posix;
-const os = std.os;
+const net = std.net;
 
 const AF_INET: u32 = 2;
 const SOCK_RAW: u32 = 3;
 const IPPROTO_TESTING: u32 = 253;
 
 pub fn main() !void {
-    
+
     // Creates a raw socket using an unused protocol
-    const sockfd: posix.socket_t = try posix.socket(AF_INET, SOCK_RAW, IPPROTO_TESTING);
-    defer posix.close(sockfd);
+    const server_socket = try posix.socket(posix.AF.INET, posix.SOCK.RAW, 253);
+    defer posix.close(server_socket);
 
-    // Set destination address (127.0.0.1)
-    var addr = posix.sockaddr.in{ //
-        .family = AF_INET,
-        .port = std.mem.nativeToBig(u16, 0), 
-        .addr = std.mem.nativeToBig(u32, 0x7F000001), // 127.0.0.1
-        .zero = [_]u8{0} ** 8,
+    // Set receive address (this is optional)
+    const addr = try net.Address.parseIp4("127.0.0.1", 4000);
+
+    // Apparently bind is optional for raw sockets? research
+    posix.bind(server_socket, &addr.any, addr.getOsSockLen()) catch |err| {
+        std.debug.print("Bind FAILED: {any}\n", .{err});
     };
-    addr = addr;
-    
-    std.debug.print("Created raw socket with file descriptor: {d}\n", .{sockfd});
 
+    std.debug.print("Waiting for packets on raw socket...\n", .{});
+    var i: u32 = 0;
+    while (true) : (i += 1) {
+        // Buffer for receiving packets
+        var buffer: [2048]u8 = undefined;
+        var src_addr: posix.sockaddr = undefined;
+        var src_addr_len: posix.socklen_t = @sizeOf(posix.sockaddr);
+
+        // Receive raw packets
+        const bytes_received = try posix.recvfrom(server_socket, &buffer, 0, &src_addr, &src_addr_len);
+
+        std.debug.print("{d} - Received {d} bytes from {any}\n", .{i, bytes_received, src_addr.data[2..6]});
+    }
 }
