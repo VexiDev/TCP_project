234aece6f2930846bc2d8b91756276d5f05b9a1a
VexiDev <79677251+VexiDev@users.noreply.github.com>
2025-03-06 17:28:27 -0500

    feat(protocol): TCP Connections can be initialized using the Connection struct init() : initializes addresses, header and opens a local socket for the given port send() : takes in some []const u8 data and prepends a serialized header in front of it before sending it to the designated destination address close() : a wrapper of posix.close() should be defered after running Connection.init()

--- src/protocol.zig
+++ src/protocol.zig
@@ -1,14 +1,68 @@
 const std = @import("std");
+const net = std.net;
+const posix = std.posix;
 const print = std.debug.print;
 
 // Header
 // - Is 5 u32 segments of 4 u8 per segment for a total of 24 u8
 
+pub const Status = union(enum) { LISTEN, SYN_SENT, SYN_RECEIVED, ESTABLISHED, FIN_WAIT_1, FIN_WAIT_2, CLOSE_WAIT, CLOSING, LAST_ACK, TIME_WAIT, CLOSED };
+
+pub const Address = struct { addr: []const u8, port: u16 };
+
+pub const Connection = struct {
+    socket: posix.socket_t,
+    status: Status = Status.CLOSED,
+    header: Header,
+    srcaddr: net.Address,
+    destaddr: net.Address,
+
+    pub fn init(src: Address, dest: Address) !Connection {
+        // parse addresses
+        const srcaddr = try net.Address.parseIp4(src.addr, src.port);
+        const destaddr = try net.Address.parseIp4(dest.addr, dest.port);
+
+        // create header
+        const header = Header{ .srcport = src.port, .destport = dest.port };
+
+        // open socket
+        const socket = try posix.socket(posix.AF.INET, posix.SOCK.RAW, 253);
+
+        return .{
+            .socket = socket,
+            .header = header,
+            .srcaddr = srcaddr,
+            .destaddr = destaddr,
+        };
+    }
+
+    pub fn send(self: *const Connection, data: []const u8) !usize {
+        // serialize header into []const u8
+        var bufr: [24]u8 = undefined;
+        _ = try self.header.serialize(&bufr);
+
+        // prepend tcp header in front of data
+        var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+        defer arena.deinit();
+        const allocator = arena.allocator();
+
+        const packet = try std.fmt.allocPrint(allocator, "{s}{s}", .{ bufr, data });
+        defer allocator.free(packet);
+        const bytes_sent = try posix.sendto(self.socket, packet, 0, &self.destaddr.any, self.destaddr.getOsSockLen());
+        std.debug.print("-> Sent {d} bytes to server\n", .{bytes_sent});
+        return bytes_sent;
+    }
+
+    pub fn close(self: *const Connection) void {
+        posix.close(self.socket);
+    }
+};
+
 pub const Header = struct {
     srcport: u16,
     destport: u16,
 
-    pub fn serialize(self: *Header, bufr: []u8) ![]const u8 {
+    pub fn serialize(self: *const Header, bufr: []u8) ![]const u8 {
         if (bufr.len < 24) return error.BufferTooSmall;
         // srcport - 2 bytes
         bufr[0] = @truncate(self.srcport >> 8);
@@ -36,12 +90,12 @@ pub const Header = struct {
     }
 };
 
-test "Test protocol" {
+test "Test header" {
     var h = Header{ .srcport = 4001, .destport = 4000 };
 
-    var buf: [24]u8 = undefined; 
+    var buf: [24]u8 = undefined;
     const serialized = try h.serialize(&buf);
-    
+
     const expected: [4]u8 = [_]u8{ 15, 161, 15, 160 };
     try std.testing.expectEqualSlices(u8, serialized[0..4], expected[0..]);
 
