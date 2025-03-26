f34e88e18fcd3051989e60e18e7bafbdb6cda9de
VexiDev <79677251+VexiDev@users.noreply.github.com>
2025-03-26 11:17:15 -0400

    feat(protocol): established structure of protocol interace
      Defined some starter functions and their expected behavior

--- src/protocol.zig
+++ src/protocol.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const posix = std.posix;
+const time = std.time;
 const print = std.debug.print;
 
 pub const Status = union(enum) { //
@@ -14,4 +15,46 @@ pub const Status = union(enum) { //
     LAST_ACK,
     TIME_WAIT,
     CLOSED,
+};
+
+const TCP = struct {
+    allocator: std.heap.ArenaAllocator,
+    // TCB_tbl:
+    // SOCK_tbl
+
+    // Initialize Protocol
+    pub fn init() TCP {
+        // Init protocol allocator
+        // Init Socket table
+        // Init TCB table
+    }
+
+    // Cleanly shutdown the protocol
+    pub fn shutdown(self: *TCP) void {
+        // Prevents new connections
+        // Resets existing connections
+        // Free all allocated memory
+        self.allocator.deinit();
+    }
+
+    // Returns a new TCP socket
+    pub fn socket() !StreamSocket {}
+
+    // Binds a socket to an address, storing it in the Socket table
+    pub fn bind() !void {}
+
+    // Requests a connection with a destination address that is listening
+    pub fn connect() !void {}
+
+    // Allows a socket to handle(i.e. queue) incoming connection requests
+    pub fn listen() !void {}
+
+    // Polls next queued connection request and establishes connection - BLOCKING
+    pub fn accept() !void {}
+
+    // Sends data to a connected destination
+    pub fn send() !usize {}
+
+    // Receives data to a connected destination
+    pub fn recv() !usize {}
 };
