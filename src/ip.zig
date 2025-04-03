f9b510258665e6e70bd14e57d6ce547e1766a2c2
VexiDev <79677251+VexiDev@users.noreply.github.com>
2025-04-03 18:15:58 -0400

    defined ip header struct

--- src/ip.zig
+++ src/ip.zig
@@ -1,3 +1,17 @@
 const std = @import("std");
 
-const IP = @This();
+const ip = struct { //
+    version: u4,
+    header_len: u4,
+    tos: u6,
+    ecn: u2,
+    total_len: u16,
+    id: u16,
+    flags: u3,
+    frag_offset: u13,
+    ttl: u8,
+    protocol: u8,
+    checksum: u16,
+    src_addr: u32,
+    dest_addr: u32,
+};
